9fbc2c6669a469f23ed582222e310d50260497d3
igor-grubyi <grubiy2011@gmail.com>
2021-04-06 15:25:37 +0300

    Added ADFS_WEB_LOGIN

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ declare module 'actamessages' {
         GROUP_SELECTED: string,
         STUDENTS_LOADED: string,
         AUTHENTICATE_USER: string,
-        AUTHENTICATION_FAILED: string
+        AUTHENTICATION_FAILED: string,
+        ADFS_WEB_LOGIN: string,
     } 
 };
